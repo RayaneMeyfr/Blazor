4f86ec5a2c15a6f96b4d7e31d64b6ee2e3a876e6
RayaneMeyfr <rayanemeyfr@hotmail.com>
2026-01-28 14:15:31 +0100

    FEAT: TPtodolist

---
+++ TPtodolist/todolist/Model/TodoTask.cs
@@ -0,0 +1,16 @@
+﻿namespace todolist.Model
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+
+       public TodoTask(int id, string title, bool isCompleted)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.IsCompleted = isCompleted;
+        }
+    }
+}
